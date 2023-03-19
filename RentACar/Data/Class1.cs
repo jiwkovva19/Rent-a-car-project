bfece136d888857d248db636f7f07868c5be22d9
Home <delyanayankova04@pmg-haskovo.org>
2023-03-19 21:47:45 +0200

    First commit

---
+++ RentACar/Data/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EGN { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
